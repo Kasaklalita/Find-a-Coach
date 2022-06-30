d7b0a7e147ffedabc65dd242cebf85a2ec0b38d6
Veniamin Polienko <kasaklalita@mail.ru>
2022-07-01 01:19:30 +0300

    Filtering Coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,6 +1,6 @@
 <template>
 	<section>
-		FILTER
+		<coach-filter @change-filter="setFilters"></coach-filter>
 	</section>
 	<section>
 		<base-card>
@@ -25,17 +25,45 @@
 
 <script>
 	import CoachItem from "../../components/coaches/CoachItem";
+	import CoachFilter from "../../components/coaches/CoachFilter";
 
 	export default {
 		components: {
-			CoachItem
+			CoachItem,
+			CoachFilter
+		},
+		data() {
+			return {
+				activeFilters: {
+					frontend: true,
+					backend: true,
+					career: true
+				}
+			};
 		},
 		computed: {
 			filteredCoaches() {
-				return this.$store.getters['coaches/coaches'];
+				const coaches = this.$store.getters['coaches/coaches'];
+				return coaches.filter(coach => {
+					if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+						return true;
+					}
+					if (this.activeFilters.backend && coach.areas.includes('backend')) {
+						return true;
+					}
+					if (this.activeFilters.career && coach.areas.includes('career')) {
+						return true;
+					}
+					return false;
+				});
 			},
 			hasCoaches() {
 				return this.$store.getters['coaches/hasCoaches'];
+			}
+		},
+		methods: {
+			setFilters(updatedFilters) {
+				this.activeFilters = updatedFilters;
 			}
 		}
 	}
