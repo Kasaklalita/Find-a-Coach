b8600623cc3b59c8b5cf251cd424b32fb31bc554
Veniamin Polienko <kasaklalita@mail.ru>
2022-07-02 01:10:03 +0300

    Sending a PUTheep Request to Store Coach Data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
-	registerCoach(context, data) {
+	async registerCoach(context, data) {
+		const userId = context.rootGetters.userId;
 		const coachData = {
 			id: context.rootGetters.userId,
 			firstName: data.first,
@@ -8,6 +9,17 @@ export default {
 			hourlyRate: data.rate,
 			areas: data.areas
 		};
-		context.commit('registerCoach', coachData);
+		const response = await fetch(`https://vue-http-demo-8e969-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+			method: 'PUT',
+			body: JSON.stringify(coachData)
+		});
+		// const responseData = await response.json();
+		if (!response.ok) {
+			// error...
+		}
+		context.commit('registerCoach', {
+			...coachData,
+			id: userId
+		});
 	}
 };
