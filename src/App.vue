ed19325a8bc06e0ca9d258cfb03486000dddbab9
Veniamin Polienko <kasaklalita@mail.ru>
2022-07-02 12:50:40 +0300

    Sending Coaching Requests Http Requests

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-	<the-header></the-header>
-	<router-view></router-view>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-	import TheHeader from "./components/layout/TheHeader";
+import TheHeader from './components/layout/TheHeader.vue';
 
-	export default {
-		components: {
-			TheHeader
-		},
-		name: "App"
-	}
+export default {
+  components: {
+    TheHeader
+  }  
+}
 </script>
 
 <style>
-	@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
-	* {
-		box-sizing: border-box;
-	}
+* {
+  box-sizing: border-box;
+}
 
-	html {
-		font-family: "Roboto", sans-serif;
-	}
+html {
+  font-family: "Roboto", sans-serif;
+}
 
-	body {
-		margin: 0;
-	}
+body {
+  margin: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue';
 
-import router from "./router"
-import store from './store/index';
-import App from "./App";
-import BaseCard from './components/ui/BaseCard';
-import BaseButton from "./components/ui/BaseButton";
-import BaseBadge from "./components/ui/BaseBadge";
-import BaseSpinner from "./components/ui/BaseSpinner";
-import BaseDialog from "./components/ui/BaseDialog";
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
 
-const app = createApp(App);
+const app = createApp(App)
 
 app.use(router);
 app.use(store);
